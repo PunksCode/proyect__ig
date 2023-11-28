575d8eac17de0bf0db9fda49311a55654bfe5913
PunksCode <agusxxpunk77@gmail.com>
2023-11-28 08:15:51 -0300

    Update 1.01

---
+++ public/js/script.js
@@ -0,0 +1,6 @@
+const btnContinuar = document.getElementById('btnContinuar');
+
+btnContinuar.addEventListener('click', function() {
+    // Redirige a la pantalla login.html
+    window.location.href = './view/login.html';
+});
